f133629c40c7d5245ce582169cf7a054fd4cf586
Anna Cole <acrrj123@gmail.com>
2023-05-22 23:27:44 -0500

    Done

--- main.js
+++ main.js
@@ -4,7 +4,40 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let hiddenClass = document.getElementById("modal");
 
+const heartButton = document.querySelectorAll(".like-glyph");
+
+function onHeartClick(likeGlyph) {
+  //const likeGlyph = e.target;
+  mimicServerCall()
+    //when on success
+    .then(function() {
+      //console.log("pass", e.target)
+        if (likeGlyph.innerText == EMPTY_HEART) {
+          likeGlyph.innerText = FULL_HEART;
+          likeGlyph.classList.add("activated-heart");
+        } else {
+          likeGlyph.innerText = EMPTY_HEART;
+          likeGlyph.classList.remove("activated-heart");
+        };
+    })
+    //if error occurs
+    .catch(function() {
+        hiddenClass.classList.remove("hidden");
+        const modalMessage = document.getElementById("modal-message")
+        modalMessage.innerHTML = "Server Error";
+
+        setTimeout(function() {
+          hiddenClass.classList.add("hidden")
+          modalMessage.innerHTML = "";
+        }, 3000);
+    });
+};
+
+for (const glyph of heartButton) {
+  glyph.addEventListener("click", () => onHeartClick(glyph));
+}
 
 
 //------------------------------------------------------------------------------
